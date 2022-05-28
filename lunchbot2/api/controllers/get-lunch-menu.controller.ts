804c91708922b21c780f44941167ffcd8ad327f0
Christian Katka <christian.katka@skillwell.fi>
2022-05-28 15:30:01 +0300

    playing around with web scraper

--- lunchbot2/api/controllers/get-lunch-menu.controller.ts
+++ lunchbot2/api/controllers/get-lunch-menu.controller.ts
@@ -1,8 +1,38 @@
+import fetch from 'cross-fetch';
 import { Context, Next } from 'koa';
+import { parse } from 'node-html-parser';
 
 export const getLunchMenu = async (ctx: Context, next: Next) => {
+  const link = 'https://www.lounaat.info/lounas/hox-city/jyvaskyla';
+
+  const res = await fetch(link);
+  const body = await res.text();
+  const data = parse(body);
+  const menu = data.querySelector('#menu');
+  if (!menu) return;
+
+  const firstDayOfTheWeek = menu.childNodes[1];
+  const firstDayOfTheWeekHeading = (
+    menu.childNodes[1].childNodes[0].childNodes[0].childNodes[0] as any
+  )._rawText;
+
+  const firstDayOfTheWeekMenuBody = firstDayOfTheWeek.childNodes[1];
+  const firstDayOfTheWeekMenu = (
+    firstDayOfTheWeekMenuBody.childNodes[0].childNodes[0].childNodes[0]
+      .childNodes[0] as any
+  )._rawText;
+
+  const firstDayOfTheWeekMenuPrice = (
+    firstDayOfTheWeekMenuBody.childNodes[0].childNodes[0].childNodes[0]
+      .childNodes[4] as any
+  )._rawText;
+
+  console.log(firstDayOfTheWeekHeading);
+  console.log(firstDayOfTheWeekMenu);
+  console.log(firstDayOfTheWeekMenuPrice);
+
   ctx.response.status = 200;
-  ctx.response.body = { key: 'jooo' };
+  ctx.response.body = { key: 'menu' };
 
   await next();
 };
